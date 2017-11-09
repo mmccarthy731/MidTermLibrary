8cf667bea5f7a874676b5a689b836dd591bc473e
Evan Ricard <evanricard11@gmail.com>
2017-11-09 16:27:14 -0500

    Reading and writing to text file.

--- MidTermLibrary/Program.cs
+++ MidTermLibrary/Program.cs
@@ -11,7 +11,6 @@ namespace MidTermLibrary
         static void Main(string[] args)
         {
             Console.WriteLine("Weclome to the Midterm Library.");
-            List<Book> books = new List<Book>();
             List<string> menuOptions = new List<string>();
 
             menuOptions.Add("1. View all books");
@@ -21,68 +20,16 @@ namespace MidTermLibrary
             menuOptions.Add("5. Donate a book to the Library");
             menuOptions.Add("6. Leave Library");
 
-            books.Add(new Book("The Odyssey", "Homer", StatusEnum.OnShelf));
-            books.Add(new Book("1984", "George Orwell", StatusEnum.OnShelf));
-            books.Add(new Book("New Moon", "Stephenie Meyer", StatusEnum.OnShelf));
-            books.Add(new Book("Life of Pi", "Yann Martel", StatusEnum.OnShelf));
-            books.Add(new Book("Brave New World", "Aldous Huxley", StatusEnum.OnShelf));
-            books.Add(new Book("Breaking Dawn", "Stephenie Meyer", StatusEnum.OnShelf));
-            books.Add(new Book("The Time Traveler's Wife", "Audrey Niffenegger", StatusEnum.OnShelf));
-            books.Add(new Book("The Great Gatsby", "F. Scott Fitzgerald", StatusEnum.OnShelf));
-            books.Add(new Book("Game of Thrones", "George R.R. Martin", StatusEnum.OnShelf));
-            books.Add(new Book("The Iliad", "Homer", StatusEnum.OnShelf));
-            books.Add(new Book("The Scarlet Letter", "Nathaniel Hawthorne", StatusEnum.OnShelf));
-            books.Add(new Book("The Adventures of Tom Sawyer", "Mark Twain", StatusEnum.OnShelf));
-            books.Add(new Book("War and Peace", "Leo Tolstoy", StatusEnum.OnShelf));
-            books.Add(new Book("Macbeth", "William Shakespeare", StatusEnum.OnShelf));
-            books.Add(new Book("Romeo and Juliet", "William Shakespeare", StatusEnum.OnShelf));
-
+            List<Book> books = TextEdit.ReadFile("library.txt");
             bool keepReading = true;
             while (keepReading)
             {
-                Console.WriteLine("\nMain Menu:\n\n1. View all books\n2. Search for a book by author\n3. Search for a book by keyword\n4. Return a checked-out book\n5. Add a book to the text log of books.\n6. Leave library\n");
-                Console.Write("Please select an option from the list above: ");
-                string input = Console.ReadLine();
-                if (input == "1") // Display all books
-                {
-                    Library.DisplayBooks(books);
-                    Library.CheckoutBook($"\nWhich book would you like to select? (Enter 1 - {books.Count} or \"M\" to return to the Main Menu): ", books);
-                    keepReading = true;
-                }
-                else if (input == "2") //Search by Author
-                {
-                    Library.SearchByAuthor("\nPlease enter the author's name (first, last or both): ", books);
-                    keepReading = true;
-                }
-                else if (input == "3") //Search by Keyword
-                {
-                    Library.SearchByKeyword("\nPlease enter a keyword to search by: ", books);
-                    keepReading = true;
-                }
-                else if (input == "4") //Return books
-                {
-                    Library.DisplayBooks(books);
-                    Library.ReturnBook($"\nPlease enter the book ID# for your return (1 - {books.Count}) or \"M\" to return to the Main Menu): ", books);
-                    keepReading = true;
-                }
-                else if (input == "5")
-                {
-                    Console.WriteLine("Add a book to the text log of books.");
-                    TextEdit.WriteBookToFile(books);
-                }
-                else if (input == "6") //Exit Program
-                {
-                    keepReading = false;
-                }
-                else
-                {
-                    Console.WriteLine("\nInvalid input.\n");
-                    keepReading = true;
-                }
                 Console.WriteLine("\nMain Menu:\n");
                 Library.DisplayMainMenu(menuOptions);
                 keepReading = Validator.GetUserChoice("Please select an option from the list above: ", menuOptions, books);
             }
+
+            TextEdit.StoreBooksToFile(books);
             Console.WriteLine("\nGoodbye!");
             Console.ReadLine();
         }
--- MidTermLibrary/TextEdit.cs
+++ MidTermLibrary/TextEdit.cs
@@ -11,130 +11,65 @@ namespace MidTermLibrary
     {
 
         private const string FILENAME = "library.txt";
-        //StreamReader read = new StreamReader(FILENAME);
-        //StreamWriter write = new StreamWriter(FILENAME, true);
         private static StreamReader readFromFILENAME;
         private static StreamWriter writeTOFILENAME;
 
-        private static string name = "Name";
-        private static string author = "Author";
-        private static StatusEnum status;
-        private static string dueDate;
-
-
-        public static void ReadFile()
+        //Option to read entire file
+        public static List<Book> ReadFile(string FILENAME)
         {
+            List<Book> books = new List<Book>();
 
             Console.WriteLine("\nViewing entire library...");
             readFromFILENAME = new StreamReader(FILENAME);
 
-            try
-            {
-                //Read each book in the file line-by-line
-                string entireFile = readFromFILENAME.ReadToEnd();
-
-                if (!readFromFILENAME.EndOfStream)
-                {
-                    return;
-                }
-
-                else
-                {
-                    //TODO: Take out debug log if this works
-                    Console.WriteLine(entireFile);
-                    CloseReadStream(readFromFILENAME);
-                }
-
-            }
-
-            catch (SystemException e)
-            {
-                Console.WriteLine("\nSomething went wrong.");
-                Console.WriteLine(e.Message + "\n");
-            }
-
-        }
-
-        //Appending individual books
-        public static void WriteBookToFile(List<Book> books)
-        {
-
-            Book book = new Book($"{name,-35}", $"{ author,-25 }", StatusEnum.OnShelf);
-
-            writeTOFILENAME = new StreamWriter(FILENAME, true);
-
-            //Garbage code
-            //Console.WriteLine("Please write books in format as follows: " + book.ToString());
-
-            Console.WriteLine("Begin entering in entries.");
-            Console.WriteLine("\nType \"save\" if you wish to save and stop adding book entries.\nType \"reset\" if you wish to reset current record of books");
-
-            //Loop through making entries until the user decides to quit
             while (true)
             {
-                try
+                string line = readFromFILENAME.ReadLine();
+
+                if (line == null || line == "")
                 {
-                    string input = Console.ReadLine();
-
-                    //TODO: Validation for input length?
-                    //if (input.Length > Book.ToString().Length)
-                    //{
-
-                    //}
-
-                    string[] split = input.Split('\t');
-
-                    ////TODO: Validation for split
-                    //if (split.Length < 1)
-                    //{
-                    //    Console.WriteLine("String must be in the correct format");
-                    //    continue;
-                    //}
-
-                    //Join everything and place a tab between each element of the split
-                    string output = string.Join("\t", split);
-
-                    //Write our output to the last line in the file
-                    writeTOFILENAME.WriteLine(output);
-
-                    if (input == "save")
-                    {
-                        books.Add(book);
-                        writeTOFILENAME.Close();
-                        break;
-                    }
-
-                    else if (input == "reset")
-                    {
-                        CloseWriteStream(writeTOFILENAME);
-                        StreamWriter reset = new StreamWriter(FILENAME, false);
-                        CloseWriteStream(reset);
-                        break;
-                    }
-
-
-                    else
-                    {
-                        continue;
-                    }
-
-
-                }
-
-                catch (SystemException e)
-                {
-                    Console.WriteLine("\nSomething went wrong trying to write to file.");
-                    Console.WriteLine(e.Message + "\n");
                     break;
                 }
+
+                string[] words = line.Split('|');
+
+                if (words.Length != 4)
+                {
+                    Console.WriteLine("Error in file format. Line contents: " + line);
+                    break;
+                }
+
+                string name = words[0];
+                string author = words[1];
+                bool success = int.TryParse(words[2], out int index);
+                if (!success)
+                {
+                    Console.WriteLine("Error in file format. Line contents: " + line);
+                    break;
+                }
+
+                StatusEnum status = (StatusEnum)index;
+                string dueDate = words[3];
+
+                books.Add(new Book(name, author, status, dueDate));
+
             }
+
+            CloseReadStream(readFromFILENAME);
+            return books;
         }
 
-        public static void StoreBooksToFile(List<Book> book)
+        public static void StoreBooksToFile(List<Book> books)
         {
             //Make a new write stream and append data
-            writeTOFILENAME = new StreamWriter(FILENAME, true);
-            writeTOFILENAME.WriteLine(book);
+            writeTOFILENAME = new StreamWriter(FILENAME, false);
+
+            foreach (Book book in books)
+            {
+                writeTOFILENAME.WriteLine($"{book.Name}|{book.Author}|{(int)book.Status}|{book.DueDate}");
+
+            }
+
             writeTOFILENAME.Close();
         }
 
